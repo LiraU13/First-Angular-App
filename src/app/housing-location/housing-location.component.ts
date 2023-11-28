cb57863bad20e61f34e7702bd83c899ef74868e8
Ulises Lira <moredialiraulises13@gmail.com>
2023-11-28 11:57:35 -0600

    12 Customize the details page

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -9,13 +9,14 @@ import {HousingLocation} from '../housinglocation';
   template: `
     <section class="listing">
       <img
-        class="listing-photo"
-        [src]="housingLocation.photo"
-        alt="Exterior photo of {{ housingLocation.name }}"
-        crossorigin
-      />
+      class="listing-photo"
+      [src]="housingLocation.photo"
+      alt="Exterior photo of {{ housingLocation.name }}"
+      crossorigin
+    />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css'],
